876bc45a8e436daa10f87d5c9ad7710fad2ec1a9
qazi112 <qarsalan13@gmail.com>
2021-01-15 19:35:18 +0500

    OpenCv Work

---
+++ open_cv/read.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+
+
+# img = cv.imread("photos/cat.jpeg")
+# cv.imshow("hello",img)
+# cv.waitKey(0)
+
+# Video reading
+# capture = cv.VideoCapture(0)
+# print(type(capture))
+# print(capture.get(3))
+# print(capture.get(4))
+# ret = capture.set(3,320)
+# while True:
+    
+#     ret,frame = capture.read()
+#     if ret:
+
+#         gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+        
+#         cv.imshow("Frame",gray)
+#         if cv.waitKey(2) & 0xFF == ord('q'):
+#             break
+       
+
+# capture.release()
+# cv.destroyAllWindows()
